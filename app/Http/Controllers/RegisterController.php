d8c11363622854d2eb37531c24cc7254c80b39bd
Leo <larias@alumno.unlam.edu.ar>
2024-12-09 18:39:45 -0300

    Agregar usuario autenticado a la redirección en el registro

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -41,6 +41,6 @@ class RegisterController extends Controller
         auth()->attempt($request->only('email', 'password'));
 
         // Redirección
-        return redirect()->route('posts.index');
+        return redirect()->route('posts.index', auth()->user());
     }
 }
